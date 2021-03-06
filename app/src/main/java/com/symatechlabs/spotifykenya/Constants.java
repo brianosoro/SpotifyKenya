1f9c9b9d81f61367c2f61a22d8c2f956b1b88e86
Brian Osoro <brianosoroinc@gmail.com>
2021-03-06 15:13:19 +0300

    Update Constants.java

--- app/src/main/java/com/symatechlabs/spotifykenya/Constants.java
+++ app/src/main/java/com/symatechlabs/spotifykenya/Constants.java
@@ -2,7 +2,7 @@ package com.symatechlabs.spotifykenya;
 
 public class Constants {
 
-    public static String CLIENT_ID = "8506e0177520431686d36beca6294e77";
+    public static String CLIENT_ID = "PUT YOUR CLIENT ID HERE";
     public static String MY_PREFS = "PREFS";
     public static String PREF_TOKEN = "ACCESS_TOKEN";
 }
